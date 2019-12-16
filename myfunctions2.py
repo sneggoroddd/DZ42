85282ae5cb1d68831389d313d6a37ce5aa23457d
sneggorod2019 <sneggorod@mail.ru>
2019-12-16 22:24:26 +0300

    myfunctions2

--- myfunctions2.py
+++ myfunctions2.py
@@ -16,3 +16,21 @@ def hello_who(who='World'):
 hello_world()
 
 hello_who("max")
+
+def pow_many(power,  *args):
+    summ = 0
+    for arg in args:
+        summ += arg
+    return (summ**power)
+    #Функция складывает любое количество цифр и возводит результат в степень power (примеры использования ниже)
+    #:param power: степень
+    #
+
+
+
+print(pow_many(1, 1, 2) == 3)  # True -> (1 + 2)**1 == 3
+print(pow_many(1, 2, 3) == 5)  # True -> (2 + 3)**1 == 5
+print(pow_many(2, 1, 1) == 4)  # True -> (1 + 1)**2 == 4
+print(pow_many(3, 2) == 8)  # True -> 2**3 == 8
+print(pow_many(2, 1, 2, 3, 4) == 100)  # True -> (1 + 2 + 3 + 4)**2 == 10**2 == 100
+
