c1f110b0d4c371f5769cb2d994bd74df8abfc56c
sneggorod2019 <sneggorod@mail.ru>
2019-12-16 21:35:07 +0300

    myfunctions

---
+++ myfunctions.py
@@ -0,0 +1,51 @@
+def simple_separator():
+    return ('*'*10)
+print(simple_separator() == '**********')  # True
+
+
+
+
+
+def long_separator(count):
+    return ('*' * count)
+    """
+    Функция создает разделитель из звездочек число которых можно регулировать параметром count
+    :param count: количество звездочек
+    :return: строка разделитель, примеры использования ниже
+    """
+print(long_separator(3) == '***')  # True
+print(long_separator(4) == '****')  # True
+
+def separator(simbol,count):
+    return (simbol* count)
+
+    """
+    Функция создает разделитель из любых символов любого количества
+    :param simbol: символ разделителя
+    :param count: количество повторений
+    :return: строка разделитель примеры использования ниже
+    """
+print(separator('-', 10) == '----------')  # True
+print(separator('#', 5) == '#####')  # True
+
+def hello_world():
+    return print('**********','\n','Hello World!','\n', '##########')
+
+
+
+    """
+    Функция печатает Hello World в формате:
+    **********
+    Hello World!
+    ##########
+    :return: None
+    """
+
+
+
+'''
+**********
+Hello World!
+##########
+'''
+hello_world()
