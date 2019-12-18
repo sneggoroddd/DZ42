acabff86bac30594d1ebd5293c910210af2d82a7
sneggorod2019 <sneggorod@mail.ru>
2019-12-18 21:07:06 +0300

    последовательность доработать

--- myfunctions2.py
+++ myfunctions2.py
@@ -58,3 +58,23 @@ is_animal --> True
 """
 print_key_val(animal='Cat', is_animal=True)
 
+
+
+def my_filter(iterable, function):
+    """
+    (Усложненое задание со *)
+    Функция фильтрует последовательность iterable и возвращает новую
+    Если function от элемента последовательности возвращает True, то элемент входит в новую последовательность иначе нет
+    (примеры ниже)
+    :param iterable: входаня последовательности
+    :param function: функция фильтрации
+    :return: новая отфильтрованная последовательность
+    """
+    pass
+
+
+print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
+print(my_filter([1, 2, 3, 4, 5], lambda x: x == 2) == [2])  # True
+print(my_filter([1, 2, 3, 4, 5], lambda x: x != 3) == [1, 2, 4, 5])  # True
+print(my_filter(['a', 'b', 'c', 'd'], lambda x: x in 'abba') == ['a', 'b'])  # True
+
