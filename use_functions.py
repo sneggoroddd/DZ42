7e427000fbeab62b51a2ad8587594568bad79858
sneggorod2019 <sneggorod@mail.ru>
2019-12-22 18:13:42 +0300

    программа ОС для платежной системы

--- use_functions.py
+++ use_functions.py
@@ -1,25 +1,25 @@
+choice = 0
+balance = 0
+pokupka = 0
 while True:
     print("1. пополнение счета")
     print("2. покупка")
-    print("3. итория покупок")
+    print("3. история покупок")
     print("4. выход")
     print("5. Баланс")
-
     choice = input('Выбери пункт меню:')
     if choice == '1':
         balance = int(input('введите сумму пополнения баланса:'))
     elif choice == '2':
         pokupka = int(input('введите сумму покупки:'))
-        balance = balance - pokupka
-        if balance < 0:
+        if balance - pokupka < 0:
             print('Недостаточно средств')
-            balance = balance + pokupka
         else:
             balance = balance - pokupka
     elif choice == '3':
         pass
     elif choice == '4':
-        break
+        print('Спасибо за пользование сервисом')
     elif choice == '5':
         print("Ваш баланс:", balance)
     else:
